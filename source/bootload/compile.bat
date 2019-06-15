34e1791178e8db0f8447c12fac6f2549ebcb4765
ruwanbandara <ruwan.se.uok@gmail.com>
2019-06-15 18:28:12 +0530

    first commit

---
+++ source/bootload/compile.bat
@@ -0,0 +1,2 @@
+
+"C:\Users\Ruwan Bandara\AppData\Local\bin\NASM\nasm.exe" -f bin "C:\Users\Ruwan Bandara\Desktop\New folder (2)\Aqua_Seven_OS-master\AQUASEVEN\source\bootload\bootload.asm" -o"C:\Users\Ruwan Bandara\Desktop\New folder (2)\Aqua_Seven_OS-master\AQUASEVEN\source\bootload\bootload.iso"
---
+++ source/qemu.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\qemu\qemu-system-x86_64.exe" "C:\Users\Ruwan Bandara\Desktop\New folder (2)\Aqua_Seven_OS-master\AQUASEVEN\source\kernel.iso"
